0ff8fcc5355f84d20f19d8998e70f06261e424c7
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2024-07-22 20:49:35 +0700

    Tách bộ lọc thành file mixins

--- Movies_Station/controllers/admin/movie.controller.js
+++ Movies_Station/controllers/admin/movie.controller.js
@@ -1,32 +1,12 @@
 const Movie = require("../../models/movie.model");
 
+const filterStatusHelper = require("../../helpers/filterStatus");
+const { query } = require("express");
+
 /* [GET] /admin/movies */
 module.exports.index = async (req, res) => {
-    let filterStatus = [
-        {
-            name: "Tất cả",
-            status: "",
-            class: " "
-        },
-        {
-            name: "Hoạt động",
-            status: "active",
-            class: " "
-        },
-        {
-            name: "Dừng hoạt động",
-            status: "inactive",
-            class: " "
-        },
-    ];
 
-    if(req.query.status){
-        const index = filterStatus.findIndex(item => item.status == req.query.status);
-        filterStatus[index].class = "active";
-    }else{
-        const index = filterStatus.findIndex(item => item.status == "");
-        filterStatus[index].class = "active";
-    }
+    const filterStatus = filterStatusHelper(req.query);
 
     let find = {
         deleted: false,
