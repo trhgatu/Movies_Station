c68865777c1fd247340a4d7abacd35a6131d76b6
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2024-07-25 12:02:48 +0700

    Tối ưu tính năng Phân trang

--- Movies_Station/controllers/admin/movie.controller.js
+++ Movies_Station/controllers/admin/movie.controller.js
@@ -1,55 +1,48 @@
-const Movie = require("../../models/movie.model");
+const Movie = require('../../models/movie.model')
 
-const filterStatusHelper = require("../../helpers/filterStatus");
-const searchHelper = require("../../helpers/search");
-const { query } = require("express");
+const filterStatusHelper = require('../../helpers/filterStatus')
+const searchHelper = require('../../helpers/search')
+const paginationHelper = require('../../helpers/pagination')
+const {query} = require('express')
 
 /* [GET] /admin/movies */
 module.exports.index = async (req, res) => {
-
-    const filterStatus = filterStatusHelper(req.query);
+    const filterStatus = filterStatusHelper(req.query)
 
     let find = {
         deleted: false,
-    };
-    if(req.query.status){
-        find.status = req.query.status;
+    }
+    if (req.query.status) {
+        find.status = req.query.status
     }
 
-    const objectSearch = searchHelper(req.query);
+    const objectSearch = searchHelper(req.query)
 
-    if(objectSearch.regex){
-        find.title = objectSearch.regex;
+    if (objectSearch.regex) {
+        find.title = objectSearch.regex
     }
 
     //Pagination
-    let objectPagination = {
-        currentPage : 1,
-        limitItems : 6
-    };
-
-    if(req.query.page){
-        objectPagination.currentPage = parseInt(req.query.page);
-    }
-
 
     const countMovies = await Movie.countDocuments(find);
-    const totalPage = Math.ceil(countMovies / objectPagination.limitItems);
-    objectPagination.totalPage = totalPage;
 
-    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems;
+    let objectPagination = paginationHelper({
+        currentPage: 1,
+        limitItems: 6,
+    },
+     req.query,
+        countMovies
+    );
+
     //End pagination
 
     const movies = await Movie.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip);
 
-
-
-
-    res.render("admin/pages/movies/index", {
-        pageTitle: "Danh sách phim",
-        movies : movies,
+    res.render('admin/pages/movies/index', {
+        pageTitle: 'Danh sách phim',
+        movies: movies,
         filterStatus: filterStatus,
-        keyword : objectSearch.keyword,
-        pagination : objectPagination
-    });
+        keyword: objectSearch.keyword,
+        pagination: objectPagination,
+    })
 }
